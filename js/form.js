12590bab9068eff92e409c7b47fd0afd25d90148
Sergey Startsev <sergey@webstartsev.ru>
2019-09-28 21:47:26 +0300

    module form.js

---
+++ js/form.js
@@ -0,0 +1,71 @@
+'use strict';
+
+(function () {
+  var MAX_ROOMS = 100;
+  var PRICE_FOR_TYPE = {'palace': 10000, 'flat': 1000, 'house': 5000, 'bungalo': 0};
+
+  var roomNumber = document.querySelector('#room_number');
+  var capacity = document.querySelector('#capacity');
+
+  var houseType = document.querySelector('#type');
+  var price = document.querySelector('#price');
+
+  var timeIn = document.querySelector('#timein');
+  var timeOut = document.querySelector('#timeout');
+
+  var syncTime = function (time) {
+    timeIn.value = time.value;
+    timeOut.value = time.value;
+  };
+
+  // Провоерка соответсвия «Количество комнат» и «Количество мест»
+  var checkCountRoomsAndPeople = function () {
+    var countRooms = roomNumber.value;
+    var countPeople = capacity.value;
+
+    roomNumber.setCustomValidity('');
+    capacity.setCustomValidity('');
+
+    if (countPeople === 0 && countRooms !== MAX_ROOMS) {
+      roomNumber.setCustomValidity('Необходимо не менее 100 комнат');
+    } else if (countRooms === MAX_ROOMS && countPeople !== 0) {
+      capacity.setCustomValidity('Только не для гостей');
+    } else if (countRooms < countPeople && countPeople !== 0) {
+      capacity.setCustomValidity('Кол-ва людей больше чем мест');
+    }
+  };
+
+  // Активация формы
+  window.form = {
+    activate: function () {
+      var adForm = document.querySelector('.ad-form');
+      adForm.classList.remove('ad-form--disabled');
+
+      var fieldsets = adForm.querySelectorAll('fieldset');
+      for (var i = 0; i < fieldsets.length; i++) {
+        fieldsets[i].disabled = false;
+      }
+    },
+  };
+
+  // События валидации «Количество комнат» и «Количество мест»
+  roomNumber.addEventListener('change', function () {
+    checkCountRoomsAndPeople();
+  });
+  capacity.addEventListener('change', function () {
+    checkCountRoomsAndPeople();
+  });
+
+  houseType.addEventListener('change', function () {
+    price.setAttribute('min', PRICE_FOR_TYPE[houseType.value]);
+  });
+
+  timeIn.addEventListener('change', function () {
+    syncTime(timeIn);
+  });
+  timeOut.addEventListener('change', function () {
+    syncTime(timeOut);
+  });
+
+  checkCountRoomsAndPeople();
+})();
